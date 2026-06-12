ff0a2d6fdae460aad0953930291a7d03c07d8fb0
Om Mane <216870751+om-mane-coder@users.noreply.github.com>
2026-06-12 08:40:13 +0530

    Initial commit: Spring Boot project initialized with Docker-Java and WebFlux

---
+++ src/main/java/com/codesandbox/engine/SandboxEngineApplication.java
@@ -0,0 +1,13 @@
+package com.codesandbox.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codesandbox/engine/SandboxEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesandbox.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
